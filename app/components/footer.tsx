58050ba239b8b6b0c76f997b291fabb92f375675
Imran-has <imranhassankhanzada@gmail.com>
2024-12-24 09:51:20 -0800

    updated code

---
+++ app/components/footer.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer className='bg-black text-white text-center p-7'>
+        <div className="flex gap-2  justify-center items-center">
+            <h1 className='text-white text-2xl font-bold'>created by imran hassan  </h1>
+            <Link href="https://www.linkedin.com/in/imran-hassan-723b841b9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">linkedln</Link>
+            <Link href="https://www.instagram.com/ihassankhan71?igsh=MTdxcmNldXNxeDlrNA==">Instagram</Link>
+            <Link href="https://github.com/Imran-has">github</Link>
+        </div>
+        <div className="flex justify-center space-x-4 gap-4">
+        <Link
+          href="/clients"
+          className="px-4 py-4 bg-red-500 text-2xl text-white rounded-lg hover:bg-blue-600 transition-all"
+        >
+          Client-Side Rendering
+        </Link>
+        <Link
+          href="/serverside"
+          className="px-4 py-4 bg-green-600 text-2xl text-white rounded-lg hover:bg-blue-600 transition-all"
+        >
+          Server-Side Rendering
+        </Link>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ app/components/header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div  className='  sm:bg-slate-400 bg-slate-600 md:
+    w-full'>
+       
+            
+       <ul className=' container  mx-auto flex justify-center items-center text-red-500 gap-4 font-semibold text-xl'>
+       <h1 className='sm: font-bold text-red-500 md:black-500 mr-20 flex  p-7 text-2xl'> ShopNow</h1>
+ 
+ 
+       <li>Home</li>     
+        <li>About</li>
+        <li> Contact</li>
+       </ul>
+    </div>
+  )
+}
+
+export default Header
